4b59f2accf9ad1875e546d6257a4f9455ae2c214
Alexandre Amourdom <alexandre.amourdom@gmail.com>
2022-11-21 14:16:37 +0100

    gestion des cycles et du reset en js

--- app.js
+++ app.js
@@ -16,8 +16,19 @@ displayTime.textContent = returnFormatedTime(restTime);
 const startPauseBtn = document.querySelector(".start-btn");
 startPauseBtn.addEventListener("click", togglePomodoro);
 
+let currentInterval = false;
+let timerID;
 function togglePomodoro() {
   handlePlayPause();
+
+  if (currentInterval) return;
+  currentInterval = true;
+
+  initialTime--;
+  displayWork.textContent = returnFormatedTime(initialTime);
+  handleClassAnimate({ work: true, rest: false });
+
+  timerID = setInterval(handleTicks, 1000);
 }
 
 let pause = true;
@@ -29,4 +40,54 @@ function handlePlayPause() {
     startPauseBtn.firstElementChild.src = "ressources/play.svg";
     pause = true;
   }
+}
+
+function handleClassAnimate(itemState) {
+  for (const item in itemState) {
+    if (itemState[item]) {
+      document.querySelector(`.${item}`).classList.add("active");
+    } else {
+      document.querySelector(`.${item}`).classList.remove("active");
+    }
+  }
+}
+
+const cycles = document.querySelector(".cycles");
+let cyclesNumber = 0;
+
+function handleTicks() {
+  if (!pause && initialTime > 0) {
+    initialTime--;
+
+    displayWork.textContent = returnFormatedTime(initialTime);
+    handleClassAnimate({ work: true, rest: false });
+  } else if (!pause && initialTime === 0 && restTime > 0) {
+    restTime--;
+    displayTime.textContent = returnFormatedTime(restTime);
+    handleClassAnimate({ work: false, rest: true });
+  } else {
+    initialTime = 1799;
+    restTime = 300;
+    displayWork.textContent = returnFormatedTime(initialTime);
+    displayTime.textContent = returnFormatedTime(restTime);
+    cyclesNumber++;
+    cyclesNumber.textContent = `Cycle(s) : ${cyclesNumber}`;
+  }
+}
+
+const resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  initialTime = 1800;
+  restTime = 300;
+  displayWork.textContent = returnFormatedTime(initialTime);
+  displayTime.textContent = returnFormatedTime(restTime);
+
+  handleClassAnimate({ work: false, rest: false });
+  startPauseBtn.firstElementChild.src = "ressources/play.svg";
+  cycles.textContent = "Cycle(s) : 0";
+
+  clearInterval(timerID);
+  currentInterval = false;
 }
